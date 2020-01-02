24de352e60081ac38594f1d6e6cc36ff620a44fc
Luiz <luiz.dubas@nu.bank>
2020-01-02 11:59:22 -0300

    Swift 5

--- Sources/JSONEncodingDetector.swift
+++ Sources/JSONEncodingDetector.swift
@@ -56,7 +56,7 @@ public struct JSONEncodingDetector {
     ////
     //// - parameter header: The front Slice of data being read and evaluated.
     //// - returns: A tuple containing the detected Unicode encoding and the lenght of the byte order mark.
-    static func detectEncoding(_ header: RandomAccessSlice<UnsafeBufferPointer<UInt8>>) -> ByteStreamPrefixInformation {
+    static func detectEncoding(_ header: Slice<UnsafeBufferPointer<UInt8>>) -> ByteStreamPrefixInformation {
 
         guard let prefix = prefixFromHeader(header) else {
             return (.utf8, 0)
@@ -82,7 +82,7 @@ public struct JSONEncodingDetector {
 
     private typealias EncodingBytePrefix = (UInt8, UInt8, UInt8?, UInt8?)
 
-    private static func prefixFromHeader(_ header: RandomAccessSlice<UnsafeBufferPointer<UInt8>>) -> EncodingBytePrefix? {
+    private static func prefixFromHeader(_ header: Slice<UnsafeBufferPointer<UInt8>>) -> EncodingBytePrefix? {
         if header.count >= 4 {
             return(header[0], header[1], header[2], header[3])
         } else if header.count >= 2 {
